bc04345470be10ba02d119050965de12c43a1ef4
112b16968 <160818741+112b16968@users.noreply.github.com>
2024-03-12 10:24:50 +0800

    Add files via upload

---
+++ Lab03JPA02.java
@@ -0,0 +1,13 @@
+public class Lab03JPA02 {
+    public static void main(String[] args) {
+        int total = 0;
+        for(int i =0; i<10 ; i+=3){
+            for(int j =2 ; j<4; j++){
+                total = j*i;
+                System.out.println("count = "+total);
+                
+            }
+         
+        }
+    }
+}
